2f97e08c368320fbc838b19c10c77315491c7e8a
ZartoW <jr.souza.2006@gmail.com>
2026-05-29 15:08:21 -0300

    feat: Code address entity

--- src/main/java/com/voltix/backend/model/Address.java
+++ src/main/java/com/voltix/backend/model/Address.java
@@ -1,5 +1,30 @@
 package com.voltix.backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Address {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zipCode;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }

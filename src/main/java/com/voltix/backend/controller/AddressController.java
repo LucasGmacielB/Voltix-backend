374cc406bb48d131344905419cd9f0396c1081f2
ZartoW <jr.souza.2006@gmail.com>
2026-05-31 20:19:52 -0300

    feat: create address REST controller

---
+++ src/main/java/com/voltix/backend/controller/AddressController.java
@@ -0,0 +1,50 @@
+package com.voltix.backend.controller;
+
+import com.voltix.backend.dto.auth.AddressDTO;
+import com.voltix.backend.model.Address;
+import com.voltix.backend.service.AddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/addresses")
+@RequiredArgsConstructor
+
+public class AddressController {
+    
+    private final AddressService addressService;
+
+    @PostMapping
+
+     public Address create(@RequestBody AddressDTO dto) {
+        return addressService.create(dto);
+    }
+
+    @GetMapping
+    public List<Address> findAll() {
+        return addressService.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public List<Address> findById() {
+        return addressService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Address findById(@PathVariable Long id) {
+        return addressService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Address update(@PathVariable Long id, @RequestBody AddressDTO dto) {
+    return addressService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        addressService.delete(id);
+}
+}
